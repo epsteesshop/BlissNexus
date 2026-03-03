5e0989cc3a4f081052ef45ece50a6bb06939f03d
Diddy <epsteesshop@gmail.com>
2026-03-02 18:18:11 -0600

    🔧 Add express.static to serve dashboard

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ const registry = new Registry(redis);
 
 // Middleware
 app.use(express.json());
+app.use(express.static(__dirname));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
